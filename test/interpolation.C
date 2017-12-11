c24a19d803dce0120498e1fe438e6ad0fc244b10
James Shaw <js102@zepler.net>
2017-12-11 15:33:43 +0000

    specify multipliers type on Istream

--- test/interpolation.C
+++ test/interpolation.C
@@ -61,7 +61,8 @@ Test::interpolation::initialiseScheme
     const word& schemeName
 )
 {
-    IStringStream schemeSpecification(schemeName + " ((0 0 0))");
+    IStringStream schemeSpecification(schemeName + " ((0 0 0)) " + 
+            "uniformMultipliers");
     return surfaceInterpolationScheme<scalar>::New
         (
             c_.mesh(),
