b24f0d42cb67583a9cd023923427ae4ccae2a64f
James Shaw <js102@zepler.net>
2017-12-14 11:45:56 +0000

    add exponentTensor.productOfExponentials()

---
+++ src/highOrderFit/interpolation/surfaceInterpolation/schemes/highOrderFit/exponentTensor.C
@@ -0,0 +1,75 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2017 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "exponentTensor.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::highOrderFit::exponentTensor::exponentTensor()
+{}
+
+
+Foam::highOrderFit::exponentTensor::exponentTensor
+(
+            const Foam::label xx,
+            const Foam::label xy,
+            const Foam::label xz,
+            const Foam::label yx,
+            const Foam::label yy,
+            const Foam::label yz,
+            const Foam::label zx,
+            const Foam::label zy,
+            const Foam::label zz
+)
+:
+    labelTensor(xx, xy, xz, yx, yy, yz, zx, zy, zz)
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::highOrderFit::exponentTensor::~exponentTensor()
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+Foam::scalar Foam::highOrderFit::exponentTensor::productOfExponentials
+(
+    const Foam::tensor A
+) const
+{
+    return pow(A.xx(), xx())
+         * pow(A.xy(), xy())
+         * pow(A.xz(), xz())
+         * pow(A.yx(), yx())
+         * pow(A.yy(), yy())
+         * pow(A.yz(), yz())
+         * pow(A.zx(), zx())
+         * pow(A.zy(), zy())
+         * pow(A.zz(), zz());
+}
+
+// ************************************************************************* //
---
+++ test/test_exponentTensor.C
@@ -0,0 +1,60 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2017 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "catch.hpp"
+#include "checks.H"
+#include "exponentTensor.H"
+
+using namespace Foam;
+
+namespace Test
+{
+
+TEST_CASE("exponentTensor_calculates_product_of_exponentials")
+{
+    const tensor A
+    (
+        10, 11, 12,
+        13, 14, 15,
+        16, 17, 18
+    );
+
+    const highOrderFit::exponentTensor K
+    (
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+    );
+
+    CHECK( K.productOfExponentials(A) ==
+            approx(13588579074408578757989397706630967514844102656e7) );
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Test
+
+
+// ************************************************************************* //
